3938a9bb0049ec6f8954fa7bae860abb844f2455
yuni666 <ber5070h@gmail.com>
2025-08-30 23:14:57 +0100

    done production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/x-icon" href="/favicon_io/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png">
+    <link rel="apple-touch-icon" href="/favicon_io/apple-touch-icon.png">
+    <link rel="manifest" href="/favicon_io/site.webmanifest">
     <title>@yield('title', '1000 Days Tracker')</title>
     <style>
         :root {
